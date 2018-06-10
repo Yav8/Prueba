bbbd35698cb7547f0b01c9e04b01f1ef814a40b3
Yav <yavieron@hotmail.es>
2018-06-10 19:09:20 +0200

    Creado proyecto prueba que contiene información relacionada con los saludos.

---
+++ Prueba1.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Prueba1 here.
+ *
+ * @author Javier de Cea Dominguez
+ * @version 10-06-2018
+ */
+public class Prueba1
+{
+    private String saludo;
+    /**
+     * Constructor for objects of class Prueba
+     */
+    public Prueba1()
+    {
+        saludo = "Buenos dias";
+    }
+}
---
+++ Prueba2.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Prueba2 here.
+ *
+ * @author Javier de Cea Dominguez
+ * @version 10-06-2018
+ */
+public class Prueba2
+{
+    private String saludo;
+    /**
+     * Constructor for objects of class Prueba2
+     */
+    public Prueba2()
+    {
+        saludo = "Buenas tardes";
+    }
+}
